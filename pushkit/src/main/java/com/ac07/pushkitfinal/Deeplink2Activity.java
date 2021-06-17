ef426db64cf182877ac09f9f0bd7a048cacd0554
AkashChandranM07 <69716122+AkashChandranM07@users.noreply.github.com>
2021-06-17 14:04:21 +0530

    OXYGEN - A Site Specific Crop Management App

---
+++ pushkit/src/main/java/com/ac07/pushkitfinal/Deeplink2Activity.java
@@ -0,0 +1,14 @@
+package com.ac07.pushkitfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deeplink2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link2);
+    }
+}
---
+++ pushkit/src/main/java/com/ac07/pushkitfinal/OnDialogClickListener.java
@@ -0,0 +1,7 @@
+package com.ac07.pushkitfinal;
+
+public interface OnDialogClickListener {
+    void onConfirmClick(String msg);
+
+    void onCancelClick();
+}
